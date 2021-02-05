2b7a984017fb47a12f1d6621f7535e5ffc237773
Sean Quillen <sean.r.quillen@gmail.com>
2021-02-05 14:43:09 -0600

    counter app tests

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import { useState } from 'react'
-import './App.css';
+import { useState } from 'react';
+import './App.css';;
 
 function App() {
-  const [count, setCounter] = useState(0)
+  const [count, setCounter] = useState(0);
+
+  const handleDecrement = () => {
+    if (count > 0) setCounter(count - 1);
+  }
   return (
     <div data-test="component__app" className="App">
       <div className="counter">
-        <div className="counter__text">{`The counter is at ${count}`}</div>
-        <button className="counter__btn" type="button" onClick={() => setCounter(count + 1)}>increment</button>
+        <span data-test="counter__text" className="counter__text">
+          The value of the count is&nbsp;
+          <span data-test="counter__count" className="counter__count">{count}</span>.
+        </span>
+        <button
+          data-test="counter__inc_btn"
+          className="counter__btn"
+          type="button"
+          onClick={() => setCounter(count + 1)}
+        >
+          increment
+        </button>
+        <button
+          data-test="counter__dec_btn"
+          className="counter__btn"
+          type="button"
+          onClick={handleDecrement}
+        >
+          decrement
+        </button>
       </div>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,28 +1,74 @@
-import Enzyme, { shallow } from 'enzyme'
-import Adapter from '@wojtekmaj/enzyme-adapter-react-17'
+import Enzyme, { shallow, ShallowWrapper } from 'enzyme';
+import Adapter from '@wojtekmaj/enzyme-adapter-react-17';
 
-import App from './App'
+import App from './App';
 
-Enzyme.configure({ adapter: new Adapter() })
+Enzyme.configure({ adapter: new Adapter() });
+
+
+/**
+ * Factory function to create a ShallowWrapper for the App component.
+ * @function setup
+ * @returns {ShallowWrapper}
+ */
+const setup = () => shallow(<App />);
+
+const findByTestAttr = (wrapper, val) => wrapper.find(`[data-test='${val}']`)
 
 test('renders App without error', () => {
-  const wrapper = shallow(<App />)
-  const appComponent = wrapper.find("[data-test='component__app']")
-  expect(appComponent.length).toBe(1)
+  const wrapper = setup();
+  const appComponent = findByTestAttr(wrapper, 'component__app');
+  expect(appComponent.length).toBe(1);
 });
 
-test("renders button", () => {
-
+test("renders counter display", () => {
+  const wrapper = setup();
+  const counterText = findByTestAttr(wrapper, 'counter__text');
+  expect(counterText.length).toBe(1);
 })
 
-test("renders counter display", () => {
+test("renders inc button", () => {
+  const wrapper = setup();
+  const counterBtn = findByTestAttr(wrapper, 'counter__inc_btn');
+  expect(counterBtn.length).toBe(1);
+})
 
+test("renders dec button", () => {
+  const wrapper = setup();
+  const counterBtn = findByTestAttr(wrapper, 'counter__dec_btn');
+  expect(counterBtn.length).toBe(1);
 })
 
 test("counter starts at 0", () => {
-
+  const wrapper = setup();
+  const count = findByTestAttr(wrapper, 'counter__count').text();
+  expect(count).toBe("0");
 })
 
 test("clicking on button increments counter", () => {
+  const wrapper = setup();
+  const counterBtn = findByTestAttr(wrapper, 'counter__inc_btn');
+  counterBtn.simulate('click');
+  const count = findByTestAttr(wrapper, 'counter__count').text();
+  expect(count).toBe("1");
+})
 
+test("clicking decrement won't go below 0", () => {
+  const wrapper = setup();
+  const decrementBtn = findByTestAttr(wrapper, 'counter__dec_btn');
+  decrementBtn.simulate('click')
+  const count = findByTestAttr(wrapper, 'counter__count').text();
+  expect(count).toBe("0");
+})
+
+test("decrement button decreases value", () => {
+  const wrapper = setup();
+  const incButton = findByTestAttr(wrapper, 'counter__inc_btn');
+  incButton.simulate('click');
+  const incrementedValue = findByTestAttr(wrapper, 'counter__count').text();
+  expect(incrementedValue).toBe("1");
+  const decButton = findByTestAttr(wrapper, 'counter__dec_btn');
+  decButton.simulate('click');
+  const decrementedValue = findByTestAttr(wrapper, 'counter__count').text();
+  expect(decrementedValue).toBe("0");
 })
