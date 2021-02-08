04c38f27fd2b7d537291df0544150538ee707dbe
Sean Quillen <sean.r.quillen@gmail.com>
2021-02-08 15:39:32 -0600

    stub basic redux tests

---
+++ src/components/Input/Input.test.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { shallow } from "enzyme";
+
+import { findByTestAttr, storeFactory } from "../../tests/testUtils";
+
+import Input from "./Input";
+import SVGButton from "../SVGButton.js";
+
+const setupInput = (initialState = {}) => {
+  const store = storeFactory(initialState);
+  return shallow(<Input store={store} />);
+};
+const setupButton = (initialState = {}) => {
+  return shallow(<SVGButton />);
+};
+
+describe("render", () => {
+  describe("word has not been guessed", () => {
+    test("renders component without error", () => {});
+    test("renders input box", () => {});
+    test("renders submit button", () => {});
+  });
+  describe("word has been guessed", () => {
+    test("renders component without error", () => {});
+    test("renders input box", () => {});
+    test("renders submit button", () => {});
+  });
+});
--- src/tests/testUtils.js
+++ src/tests/testUtils.js
@@ -1,2 +1,16 @@
+import { createStore } from 'redux';
+import rootReducer from '../store/reducers/index'
+
+/**
+ * Create a testing store with imported reducers, middleware, and inital state.
+ * globals: rootReducer
+ * @param {object} initialState - initial state from store
+ * @function storeFactory
+ * @returns {Store} - Redux store
+ */
+export const storeFactory = (initialState) => {
+  createStore(rootReducer, initialState)
+}
+
 export const findByTestAttr = (wrapper, val) =>
   wrapper.find(`[data-test='${val}']`);
